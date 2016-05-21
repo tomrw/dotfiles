cb0dab3489d03bf8e8b018ca522e963914aa6a7d
tomrw <tomrichardwest@gmail.com>
2016-05-21 12:35:42 +0100

    Remove comments and rename the create symlinks script

---
+++ create-symlinks.sh
@@ -0,0 +1,19 @@
+dir=~/dotfiles
+olddir=~/dotfiles_old
+
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+cd symlinks
+
+for file in * ; do
+	echo "Moving any existing dotfiles from ~ to $olddir"
+	mv ~/.$file ~/dotfiles_old/
+	echo "Creating symlink to $file in home directory."
+	ln -s $dir/symlinks/$file ~/.$file
+done
