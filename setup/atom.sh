2e4706a69afbe7f7c997732ad3bc9be270a4653f
Tom West <tomrichardwest@gmail.com>
2016-05-31 13:36:48 +0100

    Add atom stylesheet

--- setup/atom.sh
+++ setup/atom.sh
@@ -5,3 +5,6 @@ ln -s ~/dotfiles/atom/keymap.cson ~/.atom/keymap.cson
 
 rm ~/.atom/config.cson
 ln -s ~/dotfiles/atom/config.cson ~/.atom/config.cson
+
+rm ~/.atom/styles.less
+ln -s ~/dotfiles/atom/styles.less ~/.atom/styles.less
