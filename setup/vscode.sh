8c4015bb2331b9529475dfe0ef0089302059887b
tomrw <tomrichardwest@gmail.com>
2017-08-22 22:09:35 +0100

    Add vscode settings file

---
+++ setup/vscode.sh
@@ -0,0 +1,2 @@
+mv ~/Library/Application\ Support/Code/User/settings.json ~/Library/Application\ Support/Code/User/settings__old.json
+ln -s ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
