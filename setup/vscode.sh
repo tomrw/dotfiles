2fee4b2fc998f435fc2d437b23840fc42a480a02
Tom West <tomrichardwest@gmail.com>
2017-10-07 14:25:49 +0100

    Automate installing vs code extensions

--- setup/vscode.sh
+++ setup/vscode.sh
@@ -1,2 +1,19 @@
 mv ~/Library/Application\ Support/Code/User/settings.json ~/Library/Application\ Support/Code/User/settings__old.json
 ln -s ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+
+declare -a vsCodeExtensions=(
+	"EditorConfig.EditorConfig"
+	"alefragnani.project-manager"
+	"christian-kohler.npm-intellisense"
+	"christian-kohler.path-intellisense"
+	"dbaeumer.vscode-eslint"
+	"lukehoban.Go"
+	"ms-vscode.atom-keybindings"
+	"vscodevim.vim"
+)
+
+for extension in "${vsCodeExtensions[@]}"
+do
+	echo "Installing extension: $extension for VSCode"
+	code --install-extension $extension
+done
