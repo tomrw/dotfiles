00655161c8b81cf0d0586f7cacb3e1374a05239a
tomrw <tomrichardwest@gmail.com>
2019-07-11 17:57:44 +0100

    Configure global git config

--- setup/git.sh
+++ setup/git.sh
@@ -1,1 +1,5 @@
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+
+git config --global core.excludesfile ~/.gitignore_global
+git config --global user.name tomrw
+git config --global user.email tomrichardwest@gmail.com
