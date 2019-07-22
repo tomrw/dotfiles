f3e868e8db386211c9cd09069256462167c40107
tomrw <tomrichardwest@gmail.com>
2019-07-22 11:20:53 +0100

    Add k8 deploy command

--- scripts/k8s.sh
+++ scripts/k8s.sh
@@ -5,3 +5,18 @@ function k8validate() {
 function k8cron() {
 	kubectl get cronjob $1
 }
+
+function k8deploy() {
+	if [ "$1" = "" ]; then
+		echo "Please specify what to deploy"
+		return 0
+	fi
+
+	if [ "$2" != "" ]; then
+		echo "Deploying $1 to $2"
+		kubectl -n $2 edit deployment $1
+	else
+		echo "Deploying $1 to default"
+		kubectl -n default edit deployment $1
+	fi
+}
