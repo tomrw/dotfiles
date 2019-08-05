66d877b739b85acc9682f3263c8509a784dde06c
tomrw <tomrichardwest@gmail.com>
2019-08-05 22:19:01 +0100

    Add brew cask

--- setup/brew.sh
+++ setup/brew.sh
@@ -7,3 +7,12 @@ brew install fzf
 brew install hub
 brew install pipenv
 brew install nvm
+
+brew cask install slack
+brew cask install docker
+brew cask install visual-studio-code
+brew cask install firefox
+brew cask install iterm2
+brew cask install spotify
+brew cask install whatsapp
+brew cask install vlc
