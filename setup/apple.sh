79255401ca6be9b631efdc4e9d20684ee0b48d3c
tomrw <tomrichardwest@gmail.com>
2019-08-08 20:00:49 +0100

    Configure osx menu items

--- setup/apple.sh
+++ setup/apple.sh
@@ -33,11 +33,15 @@ defaults write com.apple.AppleMultitouchTrackpad FirstClickThreshold -int 0
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
-# Battery
+# Menu
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+defaults write com.apple.systemuiserver "NSStatusItem Visible com.apple.menuextra.bluetooth" -bool true
+defaults write com.apple.systemuiserver "NSStatusItem Visible com.apple.menuextra.volume" -bool true
+defaults write com.apple.menuextra.clock DateFormat "EEE d MMM  h:mm a"
 
 # Restart everything
 for app in "Dock" \
+	"SystemUIServer"
 	"Finder"; do
 	killall "${app}" &> /dev/null
 done
