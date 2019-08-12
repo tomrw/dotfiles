4efbd4d070f8c7539352675099cd137287b40d4a
tomrw <tomrichardwest@gmail.com>
2019-08-12 14:33:26 +0100

    Add testing snippets to vscode

--- setup/vscode.sh
+++ setup/vscode.sh
@@ -4,6 +4,9 @@ ln -s ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/s
 mv ~/Library/Application\ Support/Code/User/keybindings.json ~/Library/Application\ Support/Code/User/keybindings__old.json
 ln -s ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 
+mv ~/Library/Application\ Support/Code/User/snippets ~/Library/Application\ Support/Code/User/snippets-OLD
+ln -s ~/dotfiles/vscode/snippets ~/Library/Application\ Support/Code/User/snippets
+
 declare -a vsCodeExtensions=(
 	"alefragnani.project-manager"
 	"christian-kohler.npm-intellisense"
